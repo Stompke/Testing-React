fe4adf4cba7cda4cce0601765b54fbddd01be1e2
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-01-27 13:25:57 -0800

    project is running and testing is working

--- star-wars-app/src/setupTests.js
+++ star-wars-app/src/setupTests.js
@@ -2,4 +2,24 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import React from 'react';
+import {render, fireEvent, wait } from '@testing-library/react';
+import { getData as mockGetData } from './api';
 import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('./api');
+
+test('renders the stuff', async () => {
+    mockGetData.mockResolvedValueOnce({ id: 1});
+
+    
+
+
+
+
+})
+
+
+
+
+test('this is a asdf test', () => {})
