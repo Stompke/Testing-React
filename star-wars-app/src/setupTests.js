8ada86d906fa57889785f3d1e2b7bcc3680d8ed9
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-01-27 13:34:57 -0800

    Added test for StarWarsCharacters component, next button and previous button ;

--- star-wars-app/src/setupTests.js
+++ star-wars-app/src/setupTests.js
@@ -4,16 +4,21 @@
 // learn more: https://github.com/testing-library/jest-dom
 import React from 'react';
 import {render, fireEvent, wait } from '@testing-library/react';
+import StarWarsCharacters from './components/StarWarsCharacters';
 import { getData as mockGetData } from './api';
 import '@testing-library/jest-dom/extend-expect';
 
-jest.mock('./api');
+// jest.mock('./api');
 
-test('renders the stuff', async () => {
-    mockGetData.mockResolvedValueOnce({ id: 1});
+test('renders the StarWarsCharacters comonenet, with "Next" Button, "Previous" button, ', async () => {
+    // mockGetData.mockResolvedValueOnce({ id: 1});
 
-    
 
+    const { getByLabelText, getByText } = render(<StarWarsCharacters />);
+
+    const previousButton = getByText(/Previous/i);
+
+    const nextButton = getByText(/Next/i);
 
 
 
