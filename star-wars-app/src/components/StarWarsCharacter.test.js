cc22acffe8f58aeaab9485ca27937030bbc5c016
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-01-28 12:40:10 -0800

    async await mock ajax call is working

--- star-wars-app/src/components/StarWarsCharacter.test.js
+++
@@ -1,5 +0,0 @@
-// import React from 'react';
-// import {render, fireEvent, wait } from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
-
-// test('this is a test test', () => {})
--- star-wars-app/src/setupTests.test.js
+++ star-wars-app/src/setupTests.test.js
@@ -5,23 +5,28 @@
 import React from 'react';
 import {render, fireEvent, wait } from '@testing-library/react';
 import StarWarsCharacters from './components/StarWarsCharacters';
+import { act } from 'react-dom/test-utils';
 import { getData as mockGetData } from './api';
 import '@testing-library/jest-dom/extend-expect';
 
-// jest.mock('./api');
+jest.mock('./api');
 
 test('renders the StarWarsCharacters comonenet, with "Next" Button, "Previous" button, ', async () => {
-    // mockGetData.mockResolvedValueOnce({ id: 1});
+     mockGetData.mockResolvedValueOnce({
+       
+            count: 87,
+            next: "https://swapi.co/api/people/?page=2",
+            previous: null,
+            results: [{name:'johnny', url:'test1'}, {name:'sam', url:'test2'}, {name:'jerry', url:'test3'}, {name:'adam', url:'test4'}]
+            
+    });
 
 
-    const { getByLabelText, getByText } = render(<StarWarsCharacters />);
+        const { getByLabelText, getByText } = render(<StarWarsCharacters />);
 
-    const previousButton = getByText(/Previous/i);
-
-    const nextButton = getByText(/Next/i);
-
-
-
+        const submitButton = getByText(/Next/i);
+        
+        await wait(() => expect(getByText(/johnny/i)));
 })
 
 
